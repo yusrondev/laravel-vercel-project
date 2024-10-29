eaad625fe51629ac7adc3a460dcab4802a56ff8d
yusron <yusron.dev@gmail.com>
2024-10-29 11:12:03 +0700

    add csrf

--- resources/views/driver/create.blade.php
+++ resources/views/driver/create.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Create Driver</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
@@ -17,6 +18,7 @@
                     </div>
                     <div class="card-body">
                         <form action="{{ route('driver.store') }}" method="POST">
+                            @method('POST')
                             @csrf
                             <div class="form-group">
                                 <label for="" class="form-label">Username</label>
@@ -30,7 +32,7 @@
                                 <label for="" class="form-label">Password</label>
                                 <input type="password" class="form-control" name="password">
                             </div>
-                            <button class="btn btn-sm btn-success mt-3">Simpan</button>
+                            <button class="btn btn-sm btn-success mt-3" type="submit">Simpan</button>
                         </form>
                     </div>
                 </div>
